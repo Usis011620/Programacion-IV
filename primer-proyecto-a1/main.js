6a8b3271c2e335f4de772fca30b307e74bc786dc
Mariano Jose Ortega Hernandez <77638064+Mariano12344@users.noreply.github.com>
2022-02-09 19:18:05 -0600

    terminamos el CRUD con WebSQL y se agrego la busqueda

--- primer-proyecto-a1/main.js
+++ primer-proyecto-a1/main.js
@@ -1,12 +1,14 @@
-var db_sistema = openDatabase('dbsistema','1.0','Sistema de Facturacion',5 * 1024*1024);
+var db_sistema = openDatabase('dbsistema', '1.0', 'Sistema de Facturacion', 5 * 1024 * 1024);
 if(!db_sistema){
-    alert('Lo siento tu navegador NO soporta BD localStorage.');
+    alert('Lo siento tu navegado NO soporta BD locales.');
 }
 var app = new Vue({
     el: '#appCliente',
     data: {
+        clientes: [],
+        buscar: '',
         cliente: {
-            accion: '',
+            accion: 'nuevo',
             msg : '',
             idCliente: '',
             codigo: '',
@@ -17,36 +19,79 @@ var app = new Vue({
         },
     },
     methods: {
+        buscarCliente(){
+            /*if( this.buscar.trim().length>0 ){
+                this.clientes = this.clientes.filter(item=>item.nombre.toLowerCase().indexOf(this.buscar.toLowerCase())>=0);
+            } else {
+                this.obtenerClientes();
+            }*/
+            this.obtenerClientes(this.buscar);
+        },
         guardarCliente(){
-            db_sistema.transaction(tx=>{
-                tx.executeSql('INSERT INTO clientes (codigo, nombre, direccion, telefono, dui) VALUES (?,?,?,?,?)',
-                [this.cliente.codigo, this.cliente.nombre, this.cliente.direccion, this.cliente.telefono,
-                    this.cliente.dui],
-                    (tx, results)=>{
-                        this.cliente.msg = 'Cliente guardado con exito';
-                        this.nuevoCliente();
-                    },
-                    (tx, error)=>{
-                        this.cliente.msg = 'Error al guardar el cliente $(error.message)';
-                    });
-                });
-            },
-            nuevoCliente(){
-                this.cliente.accion = 'nuevo';
-                this.cliente.idCliente = '';
-                this.cliente.codigo = '';
-                this.cliente.nombre = '';
-                this.cliente.direccion = '';
-                this.cliente.telefono = '';
-                this.cliente.dui = '';
+            let sql = '',
+                parametros = [];
+            if(this.cliente.accion == 'nuevo'){
+                sql = 'INSERT INTO clientes (codigo, nombre, direccion, telefono, dui) VALUES (?,?,?,?,?)';
+                parametros = [this.cliente.codigo,this.cliente.nombre,this.cliente.direccion,this.cliente.telefono,this.cliente.dui];
+            }else if(this.cliente.accion == 'modificar'){
+                sql = 'UPDATE clientes SET codigo=?, nombre=?, direccion=?, telefono=?, dui=? WHERE idCliente=?';
+                parametros = [this.cliente.codigo,this.cliente.nombre,this.cliente.direccion,this.cliente.telefono,this.cliente.dui,this.cliente.idCliente];
+            }else if(this.cliente.accion == 'eliminar'){
+                sql = 'DELETE FROM clientes WHERE idCliente=?';
+                parametros = [this.cliente.idCliente];
             }
-        }, 
+            db_sistema.transaction(tx=>{
+                tx.executeSql(sql,
+                    parametros,
+                (tx, results)=>{
+                    this.cliente.msg = 'Cliente procesado con exito';
+                    this.nuevoCliente();
+                    this.obtenerClientes();
+                },
+                (tx, error)=>{
+                    this.cliente.msg = `Error al guardar el cliente ${error.message}`;
+                });
+            });
+        },
+        modificarCliente(cliente){
+            this.cliente = cliente;
+            this.cliente.accion = 'modificar';
+        },
+        eliminarCliente(cliente){
+            if( confirm(`¿Esta seguro de eliminar el cliente ${cliente.nombre}?`) ){
+                this.cliente.idCliente = cliente.idCliente;
+                this.cliente.accion = 'eliminar';
+                this.guardarCliente();
+            }
+        },
+        obtenerClientes(busqueda=''){
+            db_sistema.transaction(tx=>{
+                tx.executeSql(`SELECT * FROM clientes WHERE nombre like "%${busqueda}%" OR codigo like "%${busqueda}%"`, [], (tx, results)=>{
+                    this.clientes = results.rows;
+                    /*this.clientes = [];
+                    for(let i=0; i<results.rows.length; i++){
+                        this.clientes.push(results.rows.item(i));
+                    }*/
+                });
+            });
+        },
+        nuevoCliente(){
+            this.cliente.accion = 'nuevo';
+            this.cliente.idCliente = '';
+            this.cliente.codigo = '';
+            this.cliente.nombre = '';
+            this.cliente.direccion = '';
+            this.cliente.telefono = '';
+            this.cliente.dui = '';
+        }
+    },
     created(){
         db_sistema.transaction(tx=>{
             tx.executeSql('CREATE TABLE IF NOT EXISTS clientes(idCliente INTEGER PRIMARY KEY AUTOINCREMENT, '+
                 'codigo char(10), nombre char(75), direccion TEXT, telefono char(10), dui char(10))');
         }, err=>{
-            console.log('Error al crear la tabla cliente', err);
+            console.log('Error al crear la tabla de clientes', err);
         });
+        this.obtenerClientes();
     }
 });
